49871a2cd74c3840160566fcf0a4d25887153567
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-09 12:11:27 -0800

    add infinitely

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    """Print infinite adtion of all args"""
+    import sys
+
+    addition = 0
+    for i in range(len(sys.argv) -1):
+        addition += int(sys.argv[i + 1])
+        print("{}".format(addition))
