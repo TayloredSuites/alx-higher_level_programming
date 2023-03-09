47f63e48cf231467d9885cc37e76bfc4bec48f2f
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-09 12:20:14 -0800

    Switched import code with ns module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import *
+
 if __name__ = "__main__":
+    """Print functions for div, mul, add and sub"""
+    from calculator_1 import *
+
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
