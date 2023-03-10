4741605dacd9251f5f7594f77b0be2da2e1c874c
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-10 01:25:18 -0800

    Used return

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-""" Function that checks for lower case character """
 def islower(c):
     for c in range(65, 123, 1):
         if ord(c) < 97:
-            print("False")
+            return False
         else:
-            print("True)
+            return True
