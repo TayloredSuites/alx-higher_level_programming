ea58dd73d5d30c132e21bf5e5eadc3516b5d8863
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-09 22:43:58 -0800

    Logical operator added

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-if __name__ = "__main__":
-    """Variable that imports and prints value of module not execute"""
+if __name__ == "__main__":
     from variable_load_5 import a
 
     print(a)
