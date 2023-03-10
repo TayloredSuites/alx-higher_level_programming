cf020ffc9d0ee8bebc73020886265f1a6a503f77
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-09 22:48:31 -0800

    PEP8 Whitespace

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     import sys
 
     addition = 0
-    for i in range(len(sys.argv) -1):
+
+    for i in range(len(sys.argv) - 1):
         addition += int(sys.argv[i + 1])
         print("{}".format(addition))
