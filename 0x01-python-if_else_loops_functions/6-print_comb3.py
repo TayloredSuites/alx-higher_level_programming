4be5a06ef202904520b75a6386d4377d8a5bf0f2
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-10 01:04:33 -0800

    Prints 2 digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for first_digit in range(0, 10, 1):
+    for second_digit in range(0, 10, 1):
+        if first_digit < second_digit:
+            print("{}{}".format(first_digit, second_digit))
+        else:
+            print("Error")
