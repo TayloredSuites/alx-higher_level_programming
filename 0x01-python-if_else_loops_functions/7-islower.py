6bb365bf15d34abb3fc21e59ca09e7f0b4b5a172
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-10 01:11:27 -0800

    Lower case checker

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Function that checks for lower case character """
+def islower(c):
+    for c in range(65, 123, 1):
+        if c < chr(97):
+            print("False")
+        else:
+            print("True)
