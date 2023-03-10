55186005a14179081ec0167fb9eec8e714b998b9
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-10 01:23:05 -0800

    Made ord

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,7 +3,7 @@
 """ Function that checks for lower case character """
 def islower(c):
     for c in range(65, 123, 1):
-        if chr(c) < 97:
+        if ord(c) < 97:
             print("False")
         else:
             print("True)
