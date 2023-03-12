dd0f017ea9a68383664616474d253286d4478c53
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-12 10:29:11 -0700

    Replace list element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """ Replace a list element at a specific location """
+
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
