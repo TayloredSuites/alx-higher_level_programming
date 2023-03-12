8a7ae9a06e26dd379b7f08f142cd6687d4358d6d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-12 10:17:35 -0700

    Retrieve element from list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    for idx in range(my_list):
+            if idx < 0:
+                return None
+            elif idx > len(my_list - 1):
+                return None
+            else:
+                return my_list[idx]
