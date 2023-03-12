8987c9a7d280e4d706da96dfde4d0b161641cdc1
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-12 11:48:52 -0700

    Exclude certain elements

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """ removes all characters c and C from a string. """
+
+    new_string = []
+
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            return ("".join(new_string))
