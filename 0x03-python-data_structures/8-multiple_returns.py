67a3af50f87c1b1b1219b24ed98fa91a1a169cb0
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-14 04:51:34 -0700

    Length tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@ def multiple_returns(sentence):
     """ Returns a tuple with the length of a string and its first character """
 
     if len(sentence) == 0:
-        return sentence[0] = None
+        return None
     else:
         return (len(sentence), sentence[0])
