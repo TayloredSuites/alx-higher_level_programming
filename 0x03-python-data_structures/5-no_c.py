d9382b15442ccea280203cd713d12b64e8be9bd4
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-14 05:14:15 -0700

    Added

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,4 +7,5 @@ def no_c(my_string):
 
     for i in my_string:
         if i != 'c' and i != 'C':
-            new_string.append(i, end="")
+            new_string.append(i)
+            return new_string
