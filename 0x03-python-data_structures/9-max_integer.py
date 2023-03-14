7e2b35af88bf290944bc3558e2acd89018c09d06
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-14 04:41:12 -0700

    Max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """ Finds the biggest integer of a list """
+
+    biggest_int = my_list[0]
+
+    
+    if not my_lilst:
+        return None
+    else:
+        for i in my_list:
+            if my_list[i] > biggest_int:
+                biggest_int = my_list[i]
+                return biggest_int
