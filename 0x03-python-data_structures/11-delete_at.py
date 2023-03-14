3febebf9f6648bae7ba71213da4da10bcfe98d9d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-14 04:08:14 -0700

    Typo els else

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,6 +7,6 @@ def delete_at(my_list=[], idx=0):
         return my_list
     elif idx >= len(my_list):
         return my_list
-    els:
+    else:
         del my_list[idx]
         return my_list
