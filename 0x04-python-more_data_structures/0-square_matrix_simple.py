eb2561f846fd9e811a5461dc93deebb9da5eef3d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-15 10:34:02 -0700

    Squares

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """ Computes the square value of all integers of a matrix """
+    matrix_copy = matric.copy()
+
+    for i in matrix:
+        list(map(lambda i: i**2, matrix_copy))
