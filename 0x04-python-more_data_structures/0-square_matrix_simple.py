0db2d60915d6bcf6dd0fc0c3bdf90b71e234b6a8
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-15 10:39:36 -0700

    Squared

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 
 def square_matrix_simple(matrix=[]):
     """ Computes the square value of all integers of a matrix """
-    matrix_copy = matric.copy()
+    matrix_copy = matrix.copy()
 
     for i in matrix:
         list(map(lambda i: i**2, matrix_copy))
