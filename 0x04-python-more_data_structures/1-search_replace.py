e3f595cef5b4545baf40d777423981732bb28c4f
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-16 12:07:07 -0700

    Ehem

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,6 @@ def search_replace(my_list, search, replace):
     """ Replaces all occurrences of an element by another in a new list """
 
     new_list = my_list.copy()
-    
     for i in new_list:
         if i == search:
             i == replace
