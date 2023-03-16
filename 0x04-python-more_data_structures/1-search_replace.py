11651b20d804da7a529baf9b3691bd12faf15833
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-16 12:05:46 -0700

    Added replacement

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """ Replaces all occurrences of an element by another in a new list """
+
+    new_list = my_list.copy()
+    
+    for i in new_list:
+        if i == search:
+            i == replace
+
+    return new_list
