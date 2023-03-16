1fc33293ec579b59a7774d0be8b01bc06071d07f
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-16 11:45:53 -0700

    Concat Unknown

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,8 @@
 def no_c(my_string):
     """ removes all characters c and C from a string. """
 
-
+    ewn_lst = []
     for i in my_string:
         if i != 'c' or i != 'C':
-            return i
-        else:
-            return ''
+            ewn_list.append(i)
+            return ("".join(new_list))
