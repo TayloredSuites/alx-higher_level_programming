2d1e3b415a0a023b8b870c42d8d0eacca4410948
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-16 11:17:22 -0700

    Added 0 case

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -12,8 +12,11 @@ def divisible_by_2(my_list=[]):
             truthful = [True for i in my_list if i % 2 == 0]
             print(truthful)
             even_numbers = list(filter(lambda x: x % 2 == 0, my_list)) """
+        elif:
+            i = 0:
+                new_list.append(False)
 
         else:
             new_list.append(False)
 
-            return new_list
+    return new_list
