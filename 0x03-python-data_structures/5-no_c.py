323b715b73734232be0dfaf7efb9c6f319d7c665
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-16 11:39:48 -0700

    Returning

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,9 @@
 def no_c(my_string):
     """ removes all characters c and C from a string. """
 
+
     for i in my_string:
         if i != 'c' and i != 'C':
-
-    return i
+            return i
+        else:
+            return ''
