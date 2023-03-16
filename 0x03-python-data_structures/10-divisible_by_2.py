7e01275b47d21c7067d5f5722b4667d10b083ca1
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-16 11:23:34 -0700

    Switched if again

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,14 +6,14 @@ def divisible_by_2(my_list=[]):
     new_list = []
 
     for i in my_list:
-        if i % 2 != 0 or i == 0:
-            new_list.append(False)
+        if i % 2 == 0 or i == 0:
+            new_list.append(True)
             """ print("{} is divisible by 2".format(i))
             truthful = [True for i in my_list if i % 2 == 0]
             print(truthful)
             even_numbers = list(filter(lambda x: x % 2 == 0, my_list)) """
 
         else:
-            new_list.append(True)
+            new_list.append(False)
 
     return new_list
