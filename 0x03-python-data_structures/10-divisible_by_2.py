44969b904977601a4d401b000f3d1585b2a22527
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-16 11:06:45 -0700

    Corrected list cop filter

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,8 +5,15 @@ def divisible_by_2(my_list=[]):
 
     new_list = []
 
-    for i in range(len(my_list)):
+    for i in my_list:
         if i % 2 == 0:
             new_list.append(True)
+            """ print("{} is divisible by 2".format(i))
+            truthful = [True for i in my_list if i % 2 == 0]
+            print(truthful)
+            even_numbers = list(filter(lambda x: x % 2 == 0, my_list)) """
+
         else:
             new_list.append(False)
+
+            return new_list
