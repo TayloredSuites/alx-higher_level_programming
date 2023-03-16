935e17525d0d6c012d798bbf483885e403ed7cf6
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-16 11:37:52 -0700

    Removed new list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,10 +3,7 @@
 def no_c(my_string):
     """ removes all characters c and C from a string. """
 
-    new_string = []
-
     for i in my_string:
         if i != 'c' and i != 'C':
-            new_string.append(i+)
 
-    return new_string
+    return i
