bfeb676770910158f3f414fe13ff7a1aa01f9e4a
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-17 00:28:21 -0700

    == to =

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,6 +6,6 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in new_list:
         if i == search:
-            new_list[i] == replace
+            i = replace
 
     return new_list
