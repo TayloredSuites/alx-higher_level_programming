69785a5de59fe12bbf68a21f9bcea1ccb1b32bfe
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-17 00:26:44 -0700

    Added

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,6 +6,6 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in new_list:
         if i == search:
-            i == replace
+            new_list[i] == replace
 
     return new_list
