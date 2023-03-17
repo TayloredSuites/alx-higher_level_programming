1cbbf59b7cf36476f4ec0707fd8002b45398228e
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-17 00:22:00 -0700

    Coped matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,6 @@
 def square_matrix_simple(matrix=[]):
     """ Computes the square value of all integers of a matrix """
 
-    for i in matrix:
+    matrix_copy = matrix.copy()
+    for i in matrix_copy:
         print(list(map(lambda x: x*x, i)))
