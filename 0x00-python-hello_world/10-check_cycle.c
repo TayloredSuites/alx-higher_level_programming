8f993b8214395cb3b7f6906eb84cdaca436879b3
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-19 09:11:02 -0700

    Print to stderr

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlilb.h>
+
+/**
+ * check_cycle -  function in C that checks if a singly linked list has a cycle
+ * @list: pointer to the head node
+ *
+ * Return -  function in C that checks if a singly linked list has a cycle in it
+ */
+int check_cycle(listint_t *list)
+{
+	list = (listint_t *)malloc(sizeof(listint_t));
+
+	if (list == NULL)
+	{
+		return NULL
+	}
+	n = ;
+	next 
