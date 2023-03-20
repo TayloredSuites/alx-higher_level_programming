5124c3eb294c3f5f7bacf86b5faf10acd11efe41
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 07:23:26 -0700

    Whitespace errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for first_digit in range(0, 10, 1):
     for second_digit in range(0, 10, 1):
         if first_digit < second_digit:
-            print("{}{}".format(first_digit, second_digit), end = ", ")
+            print("{}{}".format(first_digit, second_digit), end=", ")
         else:
-            print("", end = "")
+            print("", end="")
