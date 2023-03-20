3827a6827e9999e9f7a15e45e5c1128ba8b69fe7
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 06:09:34 -0700

    Indentedd ln14

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,7 @@ elif last_dgt == 0:
     print("Last digit of {} is {} and is 0".format(number, last_dgt))
 elif last_dgt < 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format(
-        number, (last_dgt * -1))
+        number, (last_dgt * -1)))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0".format(
         number, last_dgt))
