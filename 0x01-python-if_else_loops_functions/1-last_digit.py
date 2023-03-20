1e998769cbb2ca83b2a9824625f64fc64fccbd1d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 06:17:10 -0700

    Removed abs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 import random
+
 number = random.randint(-10000, 10000)
-last_dgt = abs(number) % 10
+last_dgt = number % 10
 
 if last_dgt > 5:
     print("Last digit of {} is {} and is greater than 5".format(
