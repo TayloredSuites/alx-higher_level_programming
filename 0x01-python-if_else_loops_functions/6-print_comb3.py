313008966c7701b49fbdf3fa5e8ce2b74bcd857a
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 07:16:16 -0700

    Used end to end it

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 for first_digit in range(0, 10, 1):
     for second_digit in range(0, 10, 1):
         if first_digit < second_digit:
-            print("{}{}".format(first_digit, second_digit))
+            print("{}{}".format(first_digit, second_digit), end = ", ")
         else:
-            print("Error")
+            print("", end = "")
