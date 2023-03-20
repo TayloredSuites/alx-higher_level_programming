42ba35be9d953bcf0a2b25bb15f0e9fbe0921aeb
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 06:46:14 -0700

    Modulo giving non abs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_dgt = number % 10
+last_dgt = abs(number) % 10
 
 if number < 0:
     last_dgt = -last_dgt
@@ -13,7 +13,7 @@ if last_dgt > 5:
     print("Last digit of {} is {} and is greater than 5".format(
         number, last_dgt))
 elif last_dgt == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_dgt))
+    print("Last digit of {} is {} and is 0".format(number, last_dgt)i)
 else:
     print("Last digit of {} is {} and is less than 6 and not 0".format(
         number, last_dgt))
