512f397d7354a0e837ff22a833d6acff29221ec2
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 06:44:08 -0700

    Typo ) excluded

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,7 +13,7 @@ if last_dgt > 5:
     print("Last digit of {} is {} and is greater than 5".format(
         number, last_dgt))
 elif last_dgt == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_dgt)
+    print("Last digit of {} is {} and is 0".format(number, last_dgt))
 else:
     print("Last digit of {} is {} and is less than 6 and not 0".format(
         number, last_dgt))
