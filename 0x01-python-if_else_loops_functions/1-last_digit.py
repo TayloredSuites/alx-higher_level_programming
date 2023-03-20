331e1139c5388ce396f87decb19b395a019635fd
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 07:05:00 -0700

    Removed indent, relogicked

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if number < 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format(
         number, last_dgt))
 elif last_dgt > 5:
-        print("Last digit of {} is {} and is greater than 5".format(
+    print("Last digit of {} is {} and is greater than 5".format(
             number, last_dgt))
 elif last_dgt == 0:
     print("Last digit of {} is {} and is 0".format(number, last_dgt))
