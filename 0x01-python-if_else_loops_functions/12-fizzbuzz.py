cc17b48fb5fa5c20774a41c1aa82b3f83218f0e9
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 12:46:13 -0700

    Added func prototype

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-for i in range(1, 101, 1):
-    if i % 5 == 0 and i % 3 == 0:
-        print("FizzBuzz", end=' ')
-    elif i % 5 == 0:
-        print("Buzz", end=' ')
-    elif i % 3 == 0:
-        print("Fizz", end=' ')
-    elif i % 3 == 0 or i % 5 == 0:
-        print('')
-    else:
-        print(i, end=' ')
+def fizzbuzz():
+    for i in range(1, 101, 1):
+        if i % 5 == 0 and i % 3 == 0:
+            print("Fizzbuzz", end=' ')
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+        elif i % 3 == 0:
+            print("Fizz", end=' ')
+        elif i % 3 == 0 or i % 5 == 0:
+            print('')
+        else:
+            print(i, end=' ')
