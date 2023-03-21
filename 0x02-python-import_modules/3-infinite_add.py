de72dfd48337e74b89cfa891a9a5c786acf4f1ba
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-21 05:55:51 -0700

    Used pure len(argv) for range

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
 
     addition = 0
 
-    for i in range(len(sys.argv) - 1):
-        addition += int(sys.argv[i + 1])
-        print("{}".format(addition))
+    for i in range(1, len(sys.argv)):
+        addition += int(sys.argv[i])
+        print(addition)
