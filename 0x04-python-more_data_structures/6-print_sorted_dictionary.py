b029135270e28801c3be581b09a85aada218c2e0
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-21 09:24:15 -0700

    List element correction

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@
 def print_sorted_dictionary(a_dictionary):
     """ Function that prints a dictionary by ordered keys """
 
-    return [print("{}: {}".format(i, a_dictionary[k])) for i in
+    return [print("{}: {}".format(i, a_dictionary[i])) for i in
             sorted(a_dictionary)]
