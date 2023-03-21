f16224d45cc409a370a8faade9b92dd621020b99
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-21 08:54:18 -0700

    Added list comp

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    """ adds all unique integers in a list (only once for each integer). """
+    """ Adds all unique integers in a list (only once for each integer). """
 
     set_unique = set(my_list)
-    return (set(map(lambda x: sum(x), set_unique)))
+    return [set(map(lambda x: sum(x), set_unique))) for i in set_unique]
