4aeaa3f4ab8b5a3906441b6be886bf69ea7bfafd
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-28 10:36:14 -0700

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,9 +3,12 @@
 """ No module import allowed
 """
 
+
 class Square:
     """ Defines a square of known or unknown size
     """
+
+
     def __init__(self, size=0):
         """ Intializes the instance of size
 
@@ -15,6 +18,7 @@ class Square:
         Args:
             size (int): the size of square getter value
         """
+
         if size < 0:
             raise ValueError('size must be >= 0')
         elif type(size) is not in:
