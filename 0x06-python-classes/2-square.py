5924469b7c225551df2cd50365a677e2856173a0
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-28 11:11:10 -0700

    E303, E225 PEP8 corrected

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,6 @@ class Square:
     """ Defines a square of known or unknown size
     """
 
-
     def __init__(self, size=0):
         """ Intializes the instance of size
 
@@ -19,8 +18,7 @@ class Square:
             size (int): the size of square getter value
         """
 
-
-        if type(size) is not in:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
