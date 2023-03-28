5f6fa0d5da49ae6bfa920b34b6747f177c5ebeaa
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-28 10:56:39 -0700

    Area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" No module importation allowed
+"""
+
+class Square:
+    """ Defines a square based on 2-square.py
+    """
+
+    def __init__(self, size=0):
+        """ Initializes the instance of size
+
+        Note:
+            Private attribute is created
+
+        Args:
+            size : square size
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+
+    def area(self):
+        """ Class fuction to return current square area
+        """
+
+        print('Area: {:d}'.format(${self.__size ** 2}))
