8d860ad52c6888cf21d0ee895d24b2a85b048eb6
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-28 21:10:01 -0700

    Coordinates of a sqquare

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,6 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
-""" No module importation allowed
+""" No module imports allowed
 """
 
 
@@ -56,5 +56,5 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                for j in range in range(self.__size):
-                    print('#', end="")
+                [print'#', end="") for j in range(self.__size)]
+                print()
