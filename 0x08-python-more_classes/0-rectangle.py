2b2e3dda354311ebefde098478d78ef778d5502a
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-04-06 10:36:40 -0700

    Code added for empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" No module importation allowed """
+
+class Rectangle:
+    """ An empty class that defines a rectangle
+
+    Return:
+        Empty class
+    """
+
+    pass
